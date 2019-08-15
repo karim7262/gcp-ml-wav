ae75115f8e75666b05a7f3c8430e063c7dc93ae4
TrainerM2-hexaware+1-at-635681506020 <TrainerM2-hexaware+1-at-635681506020>
2019-08-15 20:47:09 +0530

    Separate code using functions

--- main.py
+++ main.py
@@ -9,6 +9,14 @@ from google.cloud import storage
 from google.cloud import pubsub_v1
 from google.cloud import speech_v1p1beta1 as speech
 
+def IsOverWrote(attributes,description):
+    if 'overwroteGeneration' in attributes:
+        description += '\tOverwrote generation: %s\n' % (
+            attributes['overwroteGeneration'])
+    if 'overwrittenByGeneration' in attributes:
+        description += '\tOverwritten by generation: %s\n' % (
+            attributes['overwrittenByGeneration'])
+    return description
 
 def summarize(message):
     # [START parse_message]
@@ -21,13 +29,7 @@ def summarize(message):
     generation = attributes['objectGeneration']
     description = ('\tEvent type: {event_type}\n\tBucket ID: {bucket_id}\n\tObject ID: {object_id}\n'
         '\tGeneration: {generation}\n').format(event_type=event_type,bucket_id=bucket_id,object_id=object_id,generation=generation)
-
-    if 'overwroteGeneration' in attributes:
-        description += '\tOverwrote generation: %s\n' % (
-            attributes['overwroteGeneration'])
-    if 'overwrittenByGeneration' in attributes:
-        description += '\tOverwritten by generation: %s\n' % (
-            attributes['overwrittenByGeneration'])
+    description=IsOverWrote(attributes,description)
 
     payload_format = attributes['payloadFormat']
     if payload_format == 'JSON_API_V1':
@@ -43,32 +45,39 @@ def summarize(message):
                 object_size=size,
                 metageneration=metageneration)
     if event_type=="OBJECT_FINALIZE":
-        client = speech.SpeechClient()
-        lang_dict = {'en-us': '_ENGS', 'pt-br': '_BRPS', 'es-es': '_SPLS'}
-        #bucket_name=bucket_id #"bkt-v5-628"
-        #filename=object_id
-        foldername="language-detection-test"
-        storage_client = storage.Client()
-        bucket = storage_client.get_bucket(bucket_id) #foldername+"/"+bucket_id)
-        blob = bucket.blob(object_id) #foldername+"/"+object_id) #foldername + '/' + filename)
-        destination_object_id="bkt-splitwav-destination"
-        #blob.download_to_filename(destination_object_id)
-        print("OBJECT ID IS: "+object_id+"a "+destination_object_id)
-        #newData = AudioSegment.from_wav(foldername+"/"+object_id)
-#i = 0
-#while i < len(new):
-#    outfile = new[i:i + 15000]
-#    outfile.export('./splits/trimmed_' + str(i/15000) + '.wav', format='wav')
-#    data, samplerate = soundfile.read('./splits/trimmed_' + str(i/15000) + '.wav')
-#    soundfile.write('./splits/reduced_' + str(i/15000) + '.wav', data, samplerate, subtype='PCM_16')
-#    os.remove('./splits/trimmed_' + str(i/15000) + '.wav')
-#    i += 15000
-#print('Splitting Complete!')
-        #description="Modified"
+        #Start Copy Object to Destination in Spliting Format
+        CpSrcTrgtSplits(bucket_id, object_id)
+
+        #End Copy Object to Destination in Spliting Format
     else:
        description="OtherOperation"
     return description
     # [END parse_message]
+
+
+def CpSrcTrgtSplits(bucket_id, object_id):
+    client = speech.SpeechClient()
+    lang_dict = {'en-us': '_ENGS', 'pt-br': '_BRPS', 'es-es': '_SPLS'}
+    # bucket_name=bucket_id #"bkt-v5-628"
+    # filename=object_id
+    foldername = "language-detection-test"
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_id)  # foldername+"/"+bucket_id)
+    blob = bucket.blob(object_id)  # foldername+"/"+object_id) #foldername + '/' + filename)
+    destination_object_id = "bkt-splitwav-destination"
+    # blob.download_to_filename(destination_object_id)
+    print("OBJECT ID IS: " + object_id + "a " + destination_object_id)
+    # newData = AudioSegment.from_wav(foldername+"/"+object_id)
+    # i = 0
+    # while i < len(new):
+    #    outfile = new[i:i + 15000]
+    #    outfile.export('./splits/trimmed_' + str(i/15000) + '.wav', format='wav')
+    #    data, samplerate = soundfile.read('./splits/trimmed_' + str(i/15000) + '.wav')
+    #    soundfile.write('./splits/reduced_' + str(i/15000) + '.wav', data, samplerate, subtype='PCM_16')
+    #    os.remove('./splits/trimmed_' + str(i/15000) + '.wav')
+    #    i += 15000
+    # print('Splitting Complete!')
+    # description="Modified"
 
 
 def poll_notifications(project, subscription_name):
