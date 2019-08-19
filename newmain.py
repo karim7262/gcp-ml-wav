ba7fce62c5cb0115edcd3a07867d71b73a35c92f
mujahed85 <mujahed.mca@gmail.com>
2019-08-19 14:42:11 +0530

    newmain.py without copy

--- newmain.py
+++ newmain.py
@@ -47,27 +47,30 @@ def summarize(message):
     if event_type=="OBJECT_FINALIZE":
         #Start Copy Object to Destination in Spliting Format
         #CpSrcTrgtSplits(bucket_id, object_id)
+        print("Object Uploaded...")
         destination_bucketName = "bkt-splitwav-destination-v7"
-		destination_object_id = "destination-v7" + object_id
-		copy_blob(bucket_id, object_id, destination_bucketName, destination_object_id)
-		print("****************Splitting Start****************")
-		new = AudioSegment.from_wav(destination_object_id)
-		print("****************Splitting End****************")
+	destination_object_id = "destination-v7" + object_id
+        #source_bucket = storage_client.get_bucket(bucket_id)
+        #source_blob = source_bucket.blob(object_id)
+        #new_blob = source_bucket.copy_blob(source_blob, destination_bucketName, destination_object_id)
+	#copy_blobs(bucket_id, object_id, destination_bucketName, destination_object_id)
+	print("****************Splitting Start****************")
+	#new = AudioSegment.from_wav(destination_object_id)
+	print("****************Splitting End****************")
         #End Copy Object to Destination in Spliting Format
     else:
        description="OtherOperation"
     return description
     # [END parse_message]
 
-def copy_blob(bucket_name, blob_name, new_bucket_name, new_blob_name):
+def copy_blobs(bucket_name, blob_name, new_bucket_name, new_blob_name):
     """Copies a blob from one bucket to another with a new name."""
     storage_client = storage.Client()
     source_bucket = storage_client.get_bucket(bucket_name)
     source_blob = source_bucket.blob(blob_name)
     destination_bucket = storage_client.get_bucket(new_bucket_name)
     print("Copy Start...")
-    new_blob = source_bucket.copy_blob(
-        source_blob, destination_bucket, new_blob_name)
+    new_blob = source_bucket.copy_blob(source_blob, destination_bucket, new_blob_name)
     print("Copy End")
     print('Blob {} in bucket {} copied to blob {} in bucket {}.'.format(
         source_blob.name, source_bucket.name, new_blob.name,
@@ -121,4 +124,6 @@ def poll_notifications(project, subscription_name):
 
 
 if __name__ == '__main__':
+    print("Line1: Start")
     poll_notifications("ai-proj-v7","subs-v7-1000")
+    print("LineN: End")
